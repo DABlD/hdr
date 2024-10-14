5ea466b76108e122061af7835f7da2669825f288
David Mendoza <davidmendozaofficial@gmail.com>
2024-10-14 19:56:25 +0800

    add patient package seeder
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

---
+++ database/seeders/PatientPackageSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\{Patient, PatientPackage, Package, User};
+
+class PatientPackageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $patients = Patient::all();
+
+        $types = ["APE", "PEE"];
+
+        foreach($patients as $patient){
+            $packages = Package::where('company', $patient->company_name)->get()->toArray();
+
+            if(rand(0, 1)){
+                $package = $packages[rand(0,sizeof($packages) - 1)];
+
+                $temp = new PatientPackage();
+                $temp->type = $types[rand(0, 1)];
+                $temp->user_id = $patient->user_id;
+                $temp->patient_id = $patient->id;
+                $temp->package_id = $package['id'];
+                $temp->details = json_encode($package);
+                $temp->save();
+
+                $user = User::find($patient->user_id);
+                $user->type = $temp->type;
+                $user->save();
+            }
+        }
+    }
+}
