1485323da86247e95abc8096ec2757f6c96433a8
David Mendoza <davidmendozaofficial@gmail.com>
2024-10-14 19:59:57 +0800

    tbc
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,7 +61,7 @@
                     <div class="card-header">
                         <h3 class="card-title">
                             <i class="fas fa-chart-pie mr-1"></i>
-                            Sales
+                            Reports
                         </h3>
 
                         <div class="card-tools">
