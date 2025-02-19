9c4e7fddb39954e2c8c88328e6ef81e0f2e4cbae
David Mendoza <davidmendozaofficial@gmail.com>
2025-02-19 15:13:22 +0800

    include deleted user
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/PatientPackage.php
+++ app/Models/PatientPackage.php
@@ -19,7 +19,7 @@ class PatientPackage extends Model
     }
 
     public function user(){
-        return $this->belongsTo('App\Models\User');
+        return $this->belongsTo('App\Models\User')->withTrashed();
     }
 
     public function doctor(){
