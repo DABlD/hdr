27c09446c62e011104542e77cca421d2ff761c9e
David Mendoza <davidmendozaofficial@gmail.com>
2025-03-06 17:35:20 +0800

    add classification and status
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/packagesSold.blade.php
+++ resources/views/exports/packagesSold.blade.php
@@ -22,6 +22,8 @@
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Type</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Date</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Status</th>
+			<th style="text-align: center; {{ $b }} background-color: yellow;">Classification</th>
+			<th style="text-align: center; {{ $b }} background-color: yellow;">Remarks</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -35,14 +37,28 @@
 				<td style="text-align: center;">{{ $row->user->lname }}</td>
 				<td style="text-align: center;">{{ $row->user->fname }}</td>
 				<td style="text-align: center;">{{ $row->user->gender }}</td>
-				<td style="text-align: center;">{{ isset($row->user->birthday) ? $row->user->birthday->format('F j, Y') : "-" }}</td>
+				<td style="text-align: center;">{{ isset($row->user->birthday) ? $row->user->birthday->format('M j, Y') : "-" }}</td>
 				<td style="text-align: center;">{{ isset($row->user->birthday) ? now()->diffInYears($row->user->birthday) : "-" }}</td>
 				<td style="text-align: center;">{{ $row->package->company }}</td>
 				<td style="text-align: center;">{{ $row->package->name }}</td>
 				<td style="text-align: center;">₱{{ $row->package->amount }}</td>
 				<td style="text-align: center;">{{ $row->type == "PEE" ? "PPE" : $row->type }}</td>
-				<td style="text-align: center;">{{ $row->created_at->format('F j, Y') }}</td>
+				<td style="text-align: center;">{{ $row->created_at->format('M j, Y') }}</td>
 				<td style="text-align: center;">{{ $row->status }}</td>
+				<td style="text-align: center;">
+					@if($row->classification == "Fit to work")
+						A
+					@elseif($row->classification == "Physically fit with minor illness")
+						B
+					@elseif($row->classification == "Employable but with certain impairments or conditions requiring follow-up treatment (employment is at employer's discretion)")
+						C
+					@elseif($row->classification == "Unfit to work")
+						D
+					@else
+						
+					@endif
+				</td>
+				<td style="text-align: center;">{{ $row->c_remarks }}</td>
 			</tr>
 		@endforeach
 
