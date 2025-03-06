45ea64ba97a0cc7a40afe05d9e597fd0d2f14932
David Mendoza <davidmendozaofficial@gmail.com>
2025-03-06 18:59:41 +0800

    restore deletion in queue list
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DatatableController.php
+++ app/Http/Controllers/DatatableController.php
@@ -164,12 +164,11 @@ class DatatableController extends Controller
                         $medical .=   "<a class='btn btn-warning' data-toggle='tooltip' title='Assigned Doctor' onClick='assignedDoctor($item->id)'>" .
                                         "<i class='fas fa-user-doctor'></i>" .
                                     "</a>&nbsp;";
-                    }
-                    elseif(!in_array(auth()->user()->role, ["Laboratory", "Imaging"])){
                         $medical .=   "<a class='btn btn-danger' data-toggle='tooltip' title='Delete' onClick='deleteType($item->id)'>" .
                                     "<i class='fas fa-trash'></i>" .
                                 "</a>&nbsp;";
                     }
+
                 }
                 else{
                     $medical =  "<a class='btn btn-primary' data-toggle='tooltip' title='Diagnostic Examination' onClick='evaluation($item->user_id)'>" .
