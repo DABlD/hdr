2acfc64b2a7c556578ab9d2c62cdb54a3e0d4fd8
David Mendoza <davidmendozaofficial@gmail.com>
2025-08-25 13:23:33 +0800

    new export part 1
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/exports/packagesSold.blade.php
+++ resources/views/exports/packagesSold.blade.php
@@ -4,6 +4,12 @@
 	$bc = "$b $c";
 
 	$amount = 0;
+
+	$totalPackage = array();
+	$totalGender = array();
+	$totalStatus = array();
+	$totalType = array();
+	$totalClassification = array();
 @endphp
 
 <table>
@@ -11,6 +17,7 @@
 		<tr>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">#</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Patient ID</th>
+			<th style="text-align: center; {{ $b }} background-color: yellow;">Date</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Surname</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">First Name</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Gender</th>
@@ -18,33 +25,36 @@
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Age</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Company</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Package Name</th>
-			{{-- <th style="text-align: center; {{ $b }} background-color: yellow;">Amount</th> --}}
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Type</th>
-			<th style="text-align: center; {{ $b }} background-color: yellow;">Date</th>
-			<th style="text-align: center; {{ $b }} background-color: yellow;">Status</th>
-			<th style="text-align: center; {{ $b }} background-color: yellow;">Classification</th>
 			<th style="text-align: center; {{ $b }} background-color: yellow;">Assessment</th>
+			<th style="text-align: center; {{ $b }} background-color: yellow;">Classification</th>
+			<th style="text-align: center; {{ $b }} background-color: yellow;">Status</th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($data as $row)
 			@php
 				$amount += $row->package->amount;
+
+				isset($totalPackage[strtoupper($row->package->name)]) ? $totalPackage[strtoupper($row->package->name)]++ : $totalPackage[strtoupper($row->package->name)] = 1;
+				isset($totalGender[strtoupper($row->user->gender)]) ? $totalGender[strtoupper($row->user->gender)]++ : $totalGender[strtoupper($row->user->gender)] = 1;
+				isset($totalStatus[strtoupper($row->status)]) ? $totalStatus[strtoupper($row->status)]++ : $totalStatus[strtoupper($row->status)] = 1;
+				isset($totalType[strtoupper($row->type)]) ? $totalType[strtoupper($row->type)]++ : $totalType[strtoupper($row->type)] = 1;
+				isset($totalClassification[strtoupper($row->classification)]) ? $totalClassification[strtoupper($row->classification)]++ : $totalClassification[strtoupper($row->classification)] = 1;
 			@endphp
 			<tr>
 				<td style="text-align: center;">{{ $loop->index+1 }}</td>
 				<td style="text-align: center;">{{ $row->user->patient->patient_id }}</td>
+				<td style="text-align: center;">{{ $row->created_at->format('M j, Y') }}</td>
 				<td style="text-align: center;">{{ $row->user->lname }}</td>
 				<td style="text-align: center;">{{ $row->user->fname }}</td>
 				<td style="text-align: center;">{{ $row->user->gender }}</td>
 				<td style="text-align: center;">{{ isset($row->user->birthday) ? $row->user->birthday->format('M j, Y') : "-" }}</td>
 				<td style="text-align: center;">{{ isset($row->user->birthday) ? now()->diffInYears($row->user->birthday) : "-" }}</td>
 				<td style="text-align: center;">{{ $row->package->company }}</td>
-				<td style="text-align: center;">{{ $row->package->name }}</td>
-				{{-- <td style="text-align: center;">₱{{ $row->package->amount }}</td> --}}
+				<td style="text-align: center;">{{ strtoupper($row->package->name) }}</td>
 				<td style="text-align: center;">{{ $row->type == "PEE" ? "PPE" : $row->type }}</td>
-				<td style="text-align: center;">{{ $row->created_at->format('M j, Y') }}</td>
-				<td style="text-align: center;">{{ $row->status }}</td>
+				<td style="text-align: center;">{{ $row->c_remarks }}</td>
 				<td style="text-align: center;">
 					@if($row->classification == "Fit to work")
 						A
@@ -55,17 +65,52 @@
 					@elseif($row->classification == "Unfit to work")
 						D
 					@else
-						
+						Pending
 					@endif
 				</td>
-				<td style="text-align: center;">{{ $row->c_remarks }}</td>
+				<td style="text-align: center;">{{ $row->status }}</td>
 			</tr>
 		@endforeach
 
-		<tr>
-			<td colspan="9"></td>
-			<td colspan="1" style="{{ $bc }}">₱{{ number_format($amount, 2) }}</td>
-			<td colspan="3"></td>
-		</tr>
+		@for($i = 0; $i < max(array_map('count', [$totalPackage, $totalGender, $totalStatus, $totalType, $totalClassification])); $i++)
+			<tr>
+				<td></td>
+				<td>{{ array_keys($totalPackage)[$i] }}</td>
+				<td>{{ $totalPackage[array_keys($totalPackage)[$i]] }}</td>
+				<td></td>
+				@if(isset(array_keys($totalStatus)[$i]))
+					<td>{{ array_keys($totalStatus)[$i] }}</td>
+					<td>{{ $totalStatus[array_keys($totalStatus)[$i]] }}</td>
+				@else
+					<td></td>
+					<td></td>
+				@endif
+				<td></td>
+				@if(isset(array_keys($totalClassification)[$i]))
+					<td>{{ array_keys($totalClassification)[$i] }}</td>
+					<td>{{ $totalClassification[array_keys($totalClassification)[$i]] }}</td>
+				@else
+					<td></td>
+					<td></td>
+				@endif
+				<td></td>
+				@if(isset(array_keys($totalType)[$i]))
+					<td>{{ array_keys($totalType)[$i] }}</td>
+					<td>{{ $totalType[array_keys($totalType)[$i]] }}</td>
+				@else
+					<td></td>
+					<td></td>
+				@endif
+				<td></td>
+				@if(isset(array_keys($totalGender)[$i]))
+					<td>{{ array_keys($totalGender)[$i] }}</td>
+					<td>{{ $totalGender[array_keys($totalGender)[$i]] }}</td>
+				@else
+					<td></td>
+					<td></td>
+				@endif
+				<td></td>
+			</tr>
+		@endfor
 	</tbody>
 </table>
