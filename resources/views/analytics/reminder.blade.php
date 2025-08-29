155c464bb27827b5e596b79dce9bdc456caddc8b
David Mendoza <davidmendozaofficial@gmail.com>
2025-08-29 17:57:28 +0800

    change header text, email template, and logo no background
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/analytics/reminder.blade.php
+++ resources/views/analytics/reminder.blade.php
@@ -13,17 +13,19 @@
                 <table border="0" cellpadding="0" cellspacing="0" width="600" style="background:#ffffff; border-radius:8px; overflow:hidden; box-shadow:0 2px 8px rgba(0,0,0,0.05);">
                     <!-- Header -->
                     <tr>
-                        <td align="center" bgcolor="#FFD1A9" style="padding:20px; color:#ffffff; font-size:20px; font-weight:bold;">
-                            <img src="{{ env("APP_URL") . $settings['logo'] }}" alt="Logo" height="30px" style="vertical-align: middle;">
+                        <td align="center" bgcolor="#FFD1A9" style="padding:20px; color:#0B1D39; font-size:20px; font-weight:bold;">
+                            <img src="{{ env("APP_URL") . $settings['logo'] }}" alt="Logo" height="30px" style="vertical-align: middle; mix-blend-mode: multiply;">
                             {{ ucwords(strtolower($settings['clinic_name'])) }}
                         </td>
                     </tr>
                     <!-- Body -->
                     <tr>
                         <td style="padding:30px; color:#333333; font-size:15px; line-height:1.6;">
-                            <p style="margin:0 0 15px;">Hello <strong>{{ $pp->user->fullname }}</strong></p>
                             <p style="margin:0 0 15px;">
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                Hello <strong>{{ $pp->user->fullname }},</strong>
+                            </p>
+                            <p style="margin:0 0 15px;">
+                                Good day!
                                 {{-- This is a friendly reminder for your upcoming appointment: --}}
                             </p>
                             {{-- <table cellpadding="6" cellspacing="0" width="100%" style="margin:15px 0; background:#f4f9f9; border:1px solid #e0e0e0; border-radius:6px;">
@@ -41,11 +43,13 @@
                                 </tr>
                             </table> --}}
                             <p style="margin:0 0 15px;">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                                This is a reminder from Medhealth Diagnostic Services Inc. regarding your pending APE/PPE/ECU. Please visit the clinic to complete your examination at your most convenient time. 
                                 {{-- Please arrive 10–15 minutes early and bring any required documents or lab results. --}}
                             </p>
                             <p style="margin:0 0 25px;">
-                                Kindly contact us at <a href="tel:{{ substr($settings['contact_no'], -10) }}" style="color:#4ac0c0; text-decoration:none;">{{ substr($settings['contact_no'], -10) }}</a>.
+                                Clinic Hours:
+                                <br>
+                                Monday to Friday : 8-5pm
                             </p>
                             <!-- Button -->
                             {{-- <table cellpadding="0" cellspacing="0" align="center" style="margin:20px auto;">
@@ -58,7 +62,7 @@
                                 </tr>
                             </table> --}}
                             <p style="margin:20px 0 0; font-size:13px; color:#777;">
-                                Thank you for trusting <strong>OHN</strong>.
+                                If you have any questions, concerns or want to make an appointment schedule, please don’t hesitate to get in touch with us at {{ $settings['contact_no'] }} / medhealthdiagnostics3@gmail.com
                             </p>
                         </td>
                     </tr>
