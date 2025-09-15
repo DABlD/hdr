28afe41327e397122b607a35f547536fde72f8e0
David Mendoza <davidmendozaofficial@gmail.com>
2025-09-16 01:15:09 +0800

    softdelete for pacakges
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Models/PatientPackage.php
+++ app/Models/PatientPackage.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PatientPackage extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         "user_id","patient_id","package_id","details","question_with_answers", 'remarks', 'type', 'file', 'recommendation', 'clinical_assessment', 'classification','c_remarks','doctor_id','status','vitals', 'diagnostics', 'evaluation'
     ];
 
     protected $dates = [
-        'created_at', 'updated_at'
+        'created_at', 'updated_at', 'deleted_at'
     ];
 
     public function package(){
